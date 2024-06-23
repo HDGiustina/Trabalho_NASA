d3f267e7438ad8f83531e6622e1180ffe8950c91
Edson Baierle <edsonbaierle@gmail.com>
2024-06-22 23:15:41 -0300

    Adicionando AsteriodeDAO

--- Trabalho_NASA/src/Bd/BdConexao.java
+++ Trabalho_NASA/src/Bd/BdConexao.java
@@ -4,10 +4,37 @@
  */
 package Bd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author User
  */
 public class BdConexao {
+
+    private final String driver = "com.mysql.cj.jdbc.Driver";
+    private final String url = "jdbc:mysql://localhost/" + "bancoTrabalhoPa";
+    private final String usuario = "root";
+    private final String senha = "root";
+
+    private Connection conexao;
+
+    public BdConexao() {
+        try {
+            Class.forName(driver);//carrega o driver
+
+            //abre a conexão com os parâmetros informados
+            conexao = DriverManager.getConnection(url, usuario, senha);
+
+        } catch (Exception ex) {
+            Logger.getLogger(BdConexao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
+    public Connection getConnection(){
+        return conexao;
+    }
 }
--- Trabalho_NASA/src/Model/Asteroide.java
+++ Trabalho_NASA/src/Model/Asteroide.java
@@ -9,5 +9,60 @@ package Model;
  * @author User
  */
 public class Asteroide {
+    private String nome;
+    private Float distanciaDaTerra;
+    private Boolean potencialRisco;
+    private Float velocidadeKmHr;
+    private Float tamanhoDoAsteroide;
+
+    public Asteroide(String nome, Float distanciaDaTerra, Boolean potencialRisco, Float velocidadeKmHr, Float tamanhoDoAsteroide) {
+        this.nome = nome;
+        this.distanciaDaTerra = distanciaDaTerra;
+        this.potencialRisco = potencialRisco;
+        this.velocidadeKmHr = velocidadeKmHr;
+        this.tamanhoDoAsteroide = tamanhoDoAsteroide;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Float getDistanciaDaTerra() {
+        return distanciaDaTerra;
+    }
+
+    public void setDistanciaDaTerra(Float distanciaDaTerra) {
+        this.distanciaDaTerra = distanciaDaTerra;
+    }
+
+    public Boolean getPotencialRisco() {
+        return potencialRisco;
+    }
+
+    public void setPotencialRisco(Boolean potencialRisco) {
+        this.potencialRisco = potencialRisco;
+    }
+
+    public Float getVelocidadeKmHr() {
+        return velocidadeKmHr;
+    }
+
+    public void setVelocidadeKmHr(Float velocidadeKmHr) {
+        this.velocidadeKmHr = velocidadeKmHr;
+    }
+
+    public Float getTamanhoDoAsteroide() {
+        return tamanhoDoAsteroide;
+    }
+
+    public void setTamanhoDoAsteroide(Float tamanhoDoAsteroide) {
+        this.tamanhoDoAsteroide = tamanhoDoAsteroide;
+    }
+    
+    
     
 }
